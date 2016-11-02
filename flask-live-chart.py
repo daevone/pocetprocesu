8eaf187e40b814dd793360200605757b02225a8e
student <balner.david@seznam.cz>
2016-11-02 15:04:43 +0100

    cus

--- flask-live-chart.py
+++ flask-live-chart.py
@@ -1,4 +1,5 @@
 import json
+import psutil
 from time import time
 from random import random
 from flask import Flask, render_template, make_response
@@ -11,12 +12,20 @@ def hello_world():
     return render_template('index.html', data='test')
 
 @app.route('/live-data')
+#def live_data():
+
+ #   data = [time() * 1000, psutil.cpu_times_percent(interval=None, percpu=False) [0]]
+ # response = make_response(json.dumps(data))
+  #  response.content_type = 'application/json'
+   # return response
+
+
+@app.route('/live-data')
 def live_data():
-    # Create a PHP array and echo it as JSON
-    data = [time() * 1000, random() * 100]
+    pocetprocesu=psutil.pids().__len__()
+    data = [time() * 1000, pocetprocesu]
     response = make_response(json.dumps(data))
     response.content_type = 'application/json'
     return response
-
 if __name__ == '__main__':
     app.run(debug=True, host='127.0.0.1', port=5000)
